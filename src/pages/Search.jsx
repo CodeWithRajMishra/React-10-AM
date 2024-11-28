407c57d3a616d8adc38332747acfb328d8b67ef7
CodeWithRajMishra <rajmishra3@gmail.com>
2024-11-28 14:37:22 +0530

    crud in react

---
+++ src/pages/Search.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Search=()=>{
+    return(
+        <>
+         <h1> Search Books</h1>
+        
+        </>
+    )
+}
+
+export default Search;
---
+++ src/pages/Update.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Update=()=>{
+    return(
+        <>
+         <h1> Update Books</h1>
+        
+        </>
+    )
+}
+
+export default Update;
